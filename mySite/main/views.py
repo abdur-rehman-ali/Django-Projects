9f1a3aa3359f62f50ed4737f07c879e069351a67
abdur-rehman-ali <alijoyia911@gmail.com>
2021-08-28 18:59:10 +0500

    data fetched from database

--- mySite/main/views.py
+++ mySite/main/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoListData
+
 
 # Create your views here.
 def index(request):
     return render(request,'main/index.html')
 
 def showItems(request):
-    return render(request,'main/showItem.html')
+    data=ToDoListData.objects.all()
+    return render(request,'main/showItem.html',{
+        'data':data
+    })
