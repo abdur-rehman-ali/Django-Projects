19b1fc5589ea3c742244a80373f4735d28bf145c
abdur-rehman-ali <alijoyia911@gmail.com>
2021-08-28 18:54:09 +0500

    created model class

--- mySite/main/admin.py
+++ mySite/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.ToDoListData)
+class ToDoListDataDisplay(admin.ModelAdmin):
+    list_display=['id','task']
