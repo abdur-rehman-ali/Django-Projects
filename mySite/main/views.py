23075500d0fbb56e17441a137d1319ae3b7b692a
abdur-rehman-ali <alijoyia911@gmail.com>
2021-08-28 19:27:03 +0500

    Add data from form to database

--- mySite/main/views.py
+++ mySite/main/views.py
@@ -5,7 +5,14 @@ from .forms import ToDoListForm
 
 # Create your views here.
 def index(request):
-    form=ToDoListForm(auto_id=True)
+    if request.method=="POST":
+        form = ToDoListForm(request.POST)
+        if form.is_valid():
+            task=form.cleaned_data['task']
+            data=ToDoListData(task=task)
+            data.save()
+    else:
+        form=ToDoListForm(auto_id=True)
     return render(request,'main/index.html',{
         'form':form
     })
