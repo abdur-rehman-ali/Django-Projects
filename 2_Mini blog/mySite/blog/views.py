37779a5f0b59f2a0c84a10a40d90af9691eff05a
abdur-rehman-ali <alijoyia911@gmail.com>
2021-08-31 23:15:58 +0500

    signIn form made and user added to database by signing in

--- 2_Mini blog/mySite/blog/views.py
+++ 2_Mini blog/mySite/blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponseRedirect
+from .forms import signInForm
 # Create your views here.
 def home(request):
     return render(request,'blog/home.html')
@@ -11,7 +11,16 @@ def dashboard(request):
     return render(request,'blog/dashboard.html')
 
 def signIn_view(request):
-    return render(request,'blog/signIn.html')
+    if request.method=="POST":
+        fm=signInForm(request.POST)
+        if fm.is_valid():
+            fm.save()
+            return HttpResponseRedirect('/logIn/')
+    else:
+        fm=signInForm()
+    return render(request,'blog/signIn.html',{
+        'form':fm
+    })
 
 def logIn_view(request):
     return render(request,'blog/logIn.html')
