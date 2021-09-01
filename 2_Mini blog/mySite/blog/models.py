3c9f4ac01bcae7e0b6b974feb957d8ff0debd554
abdur-rehman-ali <alijoyia911@gmail.com>
2021-09-02 01:53:20 +0500

    created model for posts

--- 2_Mini blog/mySite/blog/models.py
+++ 2_Mini blog/mySite/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=50)
+    content = models.TextField()
+    author=models.CharField(max_length=30)
+    date=models.DateTimeField()
