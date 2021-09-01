95e95445a50c9727f9db478e3d58fe861d79ae50
abdur-rehman-ali <alijoyia911@gmail.com>
2021-09-01 18:51:49 +0500

    restricted unauthorized users to access home page

--- 2_Mini blog/mySite/blog/views.py
+++ 2_Mini blog/mySite/blog/views.py
@@ -3,7 +3,10 @@ from django.contrib.auth import login,authenticate,logout
 from .forms import signInForm,logInForm
 # Create your views here.
 def home(request):
-    return render(request,'blog/home.html')
+    if request.user.is_authenticated:
+        return render(request,'blog/home.html')
+    else:
+        return HttpResponseRedirect('/logIn/')
 
 def about(request):
     return render(request,'blog/about.html')
@@ -32,16 +35,14 @@ def logIn_view(request):
     else:
         if request.method=="POST":
             fm=logInForm(request=request,data=request.POST)
-            print("POST REQUEST")
+
             print(fm.is_valid())
             if fm.is_valid():
                 uname=fm.cleaned_data['username']
                 upass=fm.cleaned_data['password']
-                print("Inside valid")
                 user=authenticate(username=uname,password=upass)
                 if user is not None:
                     login(request,user)
-                    print("Executed")
                     return HttpResponseRedirect('/')
         else:        
             fm=logInForm()
