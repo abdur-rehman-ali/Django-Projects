88c2a3685c9d0b42ec62b08ac874ba5828df43d6
abdur-rehman-ali <alijoyia911@gmail.com>
2021-09-06 02:36:35 +0500

    Form made for image input and added to database

--- 3_Image Uploader/mySite/main/views.py
+++ 3_Image Uploader/mySite/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import imageForm
 
 # Create your views here.
+def index(request):
+    if request.method=="POST":
+        form=imageForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+    form = imageForm()
+    return render(request,'main/index.html',{
+        'form':form,
+    })
