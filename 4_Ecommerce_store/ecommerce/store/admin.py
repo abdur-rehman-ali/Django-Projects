99867d5cba9bb351d889f47877b84193587aadf8
abdur-rehman-ali <alijoyia911@gmail.com>
2021-09-12 08:36:26 +0500

    models made for data

--- 4_Ecommerce_store/ecommerce/store/admin.py
+++ 4_Ecommerce_store/ecommerce/store/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.Customer)
+class customerAdmin(admin.ModelAdmin):
+    list_display=['id','user','name','email']
+
+@admin.register(models.Product)
+class productAdmin(admin.ModelAdmin):
+    list_display=['id','name','price','digital']
+
+@admin.register(models.Order)
+class orderAdmin(admin.ModelAdmin):
+    list_display=['id','customer','date_ordered','complete','transaction_id']
+
+@admin.register(models.OrderItem)
+class orderItemAdmin(admin.ModelAdmin):
+    list_display = ['id','product','order','quantity','date_added']
+
+@admin.register(models.ShippingAddress)
+class shippingAddressAdmin(admin.ModelAdmin):
+    list_display=['id','customer','order','address','city','state','zipcode','date_added']
--- 4_Ecommerce_store/ecommerce/store/models.py
+++ 4_Ecommerce_store/ecommerce/store/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import  AutoField
 
 # Create your models here.
+
+# 1-1 Relationship with user 
+class Customer(models.Model):
+    id=models.AutoField(primary_key=True)
+    user = models.OneToOneField(User,on_delete =models.CASCADE,null=True,blank=True)
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    id=models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False,null=True, blank=True)
+	# image 
+
+    def __str__(self):
+	    return self.name
+
+# 1-M relationship with customer
+class Order(models.Model):
+    id=models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+	    return str(self.id)
+
+class OrderItem(models.Model):
+    id=models.AutoField(primary_key=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+class ShippingAddress(models.Model):
+    id=models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+	    return self.address
