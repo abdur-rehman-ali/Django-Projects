ff296917072a985e640ec04c5cebd15bbbe787af
abdur-rehman-ali <alijoyia911@gmail.com>
2021-09-13 07:53:43 +0500

    added event handlers on each button

--- 4_Ecommerce_store/ecommerce/static/js/cart.js
+++ 4_Ecommerce_store/ecommerce/static/js/cart.js
@@ -1,1 +1,13 @@
-console.log('Heelo kesy ho');
+console.log('Welcome to cart');
+
+let cart_buttons = document.getElementsByClassName('cart-button');
+
+
+for (let index = 0; index < cart_buttons.length; index++) {
+    cart_buttons[index].addEventListener('click', function(){
+        let product = cart_buttons[index].getAttribute('data-product');
+        let action = cart_buttons[index].getAttribute('data-action');
+    });
+    
+}
+
