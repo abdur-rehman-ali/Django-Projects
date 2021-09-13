24b535ed80f29c4313700bc1bdea7fcd2d5bb9e8
abdur-rehman-ali <alijoyia911@gmail.com>
2021-09-13 08:04:21 +0500

    Logic made to check whether current user is logged in or not

--- 4_Ecommerce_store/ecommerce/static/js/cart.js
+++ 4_Ecommerce_store/ecommerce/static/js/cart.js
@@ -1,13 +1,20 @@
-console.log('Welcome to cart');
+console.log("Welcome to cart");
 
-let cart_buttons = document.getElementsByClassName('cart-button');
+// Targeting cart-button class 
+let cart_buttons = document.getElementsByClassName("cart-button");
 
 
+// Added click event on each button with class 'cart-button' 
 for (let index = 0; index < cart_buttons.length; index++) {
-    cart_buttons[index].addEventListener('click', function(){
-        let product = cart_buttons[index].getAttribute('data-product');
-        let action = cart_buttons[index].getAttribute('data-action');
-    });
-    
-}
+  cart_buttons[index].addEventListener("click", function () {
+    let product = cart_buttons[index].getAttribute("data-product");
+    let action = cart_buttons[index].getAttribute("data-action");
 
+    // current_user attribute is declared in main.html 
+    if (current_user == "AnonymousUser") {
+      console.log("User is not logged in");
+    } else {
+      console.log("User is logged in");
+    }
+  });
+}
