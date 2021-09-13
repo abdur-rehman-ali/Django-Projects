3a643f62f7286792dacae970a1a266c1640762a9
abdur-rehman-ali <alijoyia911@gmail.com>
2021-09-13 22:48:12 +0500

    view made for cart item

--- 4_Ecommerce_store/ecommerce/store/urls.py
+++ 4_Ecommerce_store/ecommerce/store/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 	path('', views.store, name="store"),
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
+	path('updateItem/', views.updateItem, name="updateItem"),
 
 ]
 
--- 4_Ecommerce_store/ecommerce/store/views.py
+++ 4_Ecommerce_store/ecommerce/store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
 from .models import Product,Order
 
 # Create your views here.
@@ -37,3 +38,6 @@ def checkout(request):
 			}
 	context = {'items':items,'order':order}
 	return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+	return JsonResponse('item added',safe=False)
